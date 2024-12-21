726ecb0c10fe2eb16b6348a218e03c224e4ce9e1
MichalDuvet <perina136@seznam.cz>
2024-12-21 22:46:53 +0100

    Fix boot timeout and update app to use dynamic port

--- app.py
+++ app.py
@@ -164,4 +164,5 @@ def day_content(day):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))  # Heroku will provide the port
+    app.run(host='0.0.0.0', port=port)
